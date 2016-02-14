8e88a64ff6c348e9d982a7a76da36a407f145460
jagadeesh04 <jagadeesh.g07@gmail.com>
2016-02-13 19:01:36 -0500

    Jags Professional Jaffa commit here

--- app/src/main/java/adroid/testapp/HomeScreen.java
+++ app/src/main/java/adroid/testapp/HomeScreen.java
@@ -24,7 +24,8 @@ public class HomeScreen extends AppCompatActivity {
             public void onClick(View view) {
 
                 System.out.println("Mahesh Commit here");
-                System.out.println("Jag commit here");
+                System.out.println("Jag commit hererererrere here");
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
